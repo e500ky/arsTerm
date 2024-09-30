9ca195ca6edb4334ad7596a05c3ef004813cefdd
e500 <84281538+e500ky@users.noreply.github.com>
2024-09-30 21:17:09 +0300

    Update main.py

--- main.py
+++ main.py
@@ -187,6 +187,8 @@ class terminal:
                 import setup
                 os.system("cls")
                 print(" ~ "+Fore.GREEN+"Terminal updated successfully.\n"+Style.RESET_ALL+"   Restart terminal.")
+                print("\nPress anykey to exit...")
+                break
 
             elif self.command.startswith("cat"):
                 file = self.command.split(" ")[1]
