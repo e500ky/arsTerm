f4688e0dc3494e1bba8efd46215c21b758c78d24
e500 <84281538+e500ky@users.noreply.github.com>
2024-09-30 20:26:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,46 +1,47 @@
-try:
-    from traceback import print_tb
-    from colorama import Back, Fore, Style
-    import os
-    import json
-    import time
-    from rich.console import Console
-    from rich.table import Table
-    from rich import box
-    from rich.syntax import Syntax
-    import requests
-    from bs4 import BeautifulSoup
 
-    console = Console()
+from traceback import print_tb
+from colorama import Back, Fore, Style
+import os
+import json
+import time
+from rich.console import Console
+from rich.table import Table
+from rich import box
+from rich.syntax import Syntax
+import requests
+from bs4 import BeautifulSoup
 
-    class terminal:
-        def __init__(self):
-            os.system("cls")
-            self.projectCD = os.getcwd()
-            while True:
-                with open("C:/Terminal/user.json", encoding="utf-8") as f:
-                    self.user = json.load(f)
-                    f.close()
-                if self.user["name"] == "None":
-                    self.newStart()
-                with open("C:/Terminal/user.json", encoding="utf-8") as f:
-                    self.user = json.load(f)
-                    self.username = self.user["name"]
-                    f.close()
-                with open("C:/Terminal/github.json", encoding="utf-8") as f:
-                    self.github_get = json.load(f)
-                    f.close()
+console = Console()
 
-                self.current_directory = os.getcwd()
-                print(Fore.LIGHTYELLOW_EX+f"({self.current_directory}) "+Fore.LIGHTGREEN_EX+"("+self.username+") "+Fore.LIGHTBLUE_EX+"prompt "+Fore.WHITE+"$ ", end="")
-                self.command = input("")
-                self.return_ = self.getCommand(self.command)
-                if self.return_ is not None: pass
-                if self.return_ == "exit": break
+class terminal:
+    def __init__(self):
+        os.system("cls")
+        self.projectCD = os.getcwd()
+        while True:
+            with open("C:/Terminal/user.json", encoding="utf-8") as f:
+                self.user = json.load(f)
+                f.close()
+            if self.user["name"] == "None":
+                self.newStart()
+            with open("C:/Terminal/user.json", encoding="utf-8") as f:
+                self.user = json.load(f)
+                self.username = self.user["name"]
+                f.close()
+            with open("C:/Terminal/github.json", encoding="utf-8") as f:
+                self.github_get = json.load(f)
+                f.close()
+
+            self.current_directory = os.getcwd()
+            print(Fore.LIGHTYELLOW_EX+f"({self.current_directory}) "+Fore.LIGHTGREEN_EX+"("+self.username+") "+Fore.LIGHTBLUE_EX+"prompt "+Fore.WHITE+"$ ", end="")
+            self.command = input("")
+            self.return_ = self.getCommand(self.command)
+            if self.return_ is not None: pass
+            if self.return_ == "exit": break
 
 
-        def getCommand(self, command):
-            self.command = command
+    def getCommand(self, command):
+        self.command = command
+        try:
             if self.command.startswith("cmd"):
                 try:
                     print("\n"+Fore.LIGHTBLUE_EX+"CMD↴ "+Style.RESET_ALL+"\n")
@@ -110,7 +111,7 @@ try:
         <script src="script.js"></script>
     </head>
     <body>
-        
+            
     </body>
     </html>
     """)
@@ -119,7 +120,7 @@ try:
                         with open(f"{project_name}/LICENSE", "w", encoding="utf-8") as f:
                             f.write("MIT License\n\n")
                             f.close()
-                        
+                            
                         with open(f"{project_name}/README.md", "w", encoding="utf-8") as f:
                             f.write("# "+project_name+"\n")
                             f.close()
@@ -127,7 +128,7 @@ try:
                         with open(f"{project_name}/style.css", "w", encoding="utf-8") as f:
                             f.write("""/* Your CSS goes here */""")
                             f.close()
-                        
+                            
                         with open(f"{project_name}/script.js", "w", encoding="utf-8") as f:
                             f.write("""// Your JavaScript goes here...""")
                             f.close()
@@ -136,7 +137,7 @@ try:
                         print(f" ~ You can open your project by typing 'open {project_name}'.")
                 except Exception as e:
                     print(Fore.RED+"Error: "+str(e))
-            
+                
             elif self.command.startswith("open"):
                 project_name = self.command.split(" ")[1]
                 try:
@@ -159,15 +160,15 @@ try:
                     print(Fore.RED+"Error: "+str(e))
 
             elif self.command.startswith("pwd"): print(Fore.LIGHTYELLOW_EX+"Current working directory: "+Fore.WHITE+self.current_directory)
-
+            
             elif self.command.startswith("cd"):
                 try:
                     directory = self.command.split(" ")[1]
                     os.chdir(directory)
                     self.current_directory = os.getcwd()
                     with open("C:/Terminal/config.json", "w", encoding="utf-8") as f:
-                        json.dump({"$default.folder": self.current_directory}, f, ensure_ascii=False)
-                        f.close()
+                            json.dump({"$default.folder": self.current_directory}, f, ensure_ascii=False)
+                            f.close()
                 except Exception as e:
                     print(Fore.RED+"Error: "+str(e))
 
@@ -184,7 +185,7 @@ try:
                 os.system("cls")
                 import setup
                 os.system("cls")
-                print(" ~ "+Fore.GREEN+"Terminal updated successfully.\n"+Style.RESET_ALL+"   Restart terminal.")
+                print(" ~ "+Fore.GREEN+"Terminal updated successfully.\n"+Style.RESET_ALL+"   Restar terminal.")
 
             elif self.command.startswith("cat"):
                 file = self.command.split(" ")[1]
@@ -211,7 +212,6 @@ try:
                     print(Fore.RED+"Error: File not found.")
 
             elif self.command.startswith("github"):
-
                 action = self.command.split(" ")[1]
                 if action == "create":
                     if self.github_get["token"] == "None":
@@ -227,17 +227,17 @@ try:
                     print(Fore.LIGHTYELLOW_EX+"Repo State (Private[Y]/Public[n]): "+Style.RESET_ALL, end="")
                     self.rstate = input()
                     while True:
-                            if self.rstate == "Y" or self.rstate == "y":
-                                self.rstate = True
-                                break
-                            elif self.rstate == "N" or self.rstate == "n":
-                                self.rstate = False
-                                break
-                            else:
-                                print(Fore.RED+"Error: Invalid choice. Please enter 'Y' for Private or 'N' for Public.")
-                                print(Fore.LIGHTYELLOW_EX+"Repo State (Private[Y]/Public[n]): "+Style.RESET_ALL, end="")
-                                self.rstate = input()
-                                continue
+                        if self.rstate == "Y" or self.rstate == "y":
+                            self.rstate = True
+                            break
+                        elif self.rstate == "N" or self.rstate == "n":
+                            self.rstate = False
+                            break
+                        else:
+                            print(Fore.RED+"Error: Invalid choice. Please enter 'Y' for Private or 'N' for Public.")
+                            print(Fore.LIGHTYELLOW_EX+"Repo State (Private[Y]/Public[n]): "+Style.RESET_ALL, end="")
+                            self.rstate = input()
+                            continue
                     self.create_github_repo(token=token, owner=uname, repo_name=self.rname, state=self.rstate)
                 elif action == "delete":
                     if self.github_get["token"] == "None":
@@ -301,83 +301,82 @@ try:
 
             if self.command != "" and self.command != "reset" and self.command != "clear":
                 print()
+        except Exception as e:
+            print(Fore.RED+"Error: "+str(e))
 
-        def listFilesAndDirectories(self):
-            try:
-                files = os.listdir()
-                print()
-                for file in files:
-                    if os.path.isdir(file):
-                        print(Fore.YELLOW+" / "+Fore.LIGHTBLACK_EX+file)
-                    else:
-                        print(Fore.CYAN+" ~ "+Fore.WHITE+file)
-            except Exception as e:
-                print(Fore.RED+"Error: "+str(e))
+    def listFilesAndDirectories(self):
+        try:
+            files = os.listdir()
+            print()
+            for file in files:
+                if os.path.isdir(file):
+                    print(Fore.YELLOW+" / "+Fore.LIGHTBLACK_EX+file)
+                else:
+                    print(Fore.CYAN+" ~ "+Fore.WHITE+file)
+        except Exception as e:
+            print(Fore.RED+"Error: "+str(e))
 
-        def resetTerm(self):
-            self.user["name"] = "None"
-            self.delGit = {
-                "token": "None",
-                "name": "None"
-            }
-            with open("C:/Terminal/user.json", "w", encoding="utf-8") as f:
-                json.dump(self.user, f, ensure_ascii=False)
-                f.close()
-            with open("C:/Terminal/github.json", "w", encoding="utf-8") as f:
-                json.dump(self.delGit, f, ensure_ascii=False)
-                f.close()
-            os.system("cls")
-            print("Terminal resetted.")
-            time.sleep(1)
-            os.system("cls")
-            self.newStart()
+    def resetTerm(self):
+        self.user["name"] = "None"
+        self.delGit = {
+            "token": "None",
+            "name": "None"
+        }
+        with open("C:/Terminal/user.json", "w", encoding="utf-8") as f:
+            json.dump(self.user, f, ensure_ascii=False)
+            f.close()
+        with open("C:/Terminal/github.json", "w", encoding="utf-8") as f:
+            json.dump(self.delGit, f, ensure_ascii=False)
+            f.close()
+        os.system("cls")
+        print("Terminal resetted.")
+        time.sleep(1)
+        os.system("cls")
+        self.newStart()
 
-        def newStart(self):
-            print(Fore.LIGHTYELLOW_EX+"NAME: "+Style.RESET_ALL, end="")
-            self.uname = input()
-            with open("C:/Terminal/user.json", "w", encoding="utf-8") as f:
-                json.dump({"name": self.uname}, f, ensure_ascii=False)
-                f.close()
-            os.system("cls")
-            print("Welcome, "+self.uname+"!")
-            time.sleep(1)
-            os.system("cls")
+    def newStart(self):
+        print(Fore.LIGHTYELLOW_EX+"NAME: "+Style.RESET_ALL, end="")
+        self.uname = input()
+        with open("C:/Terminal/user.json", "w", encoding="utf-8") as f:
+            json.dump({"name": self.uname}, f, ensure_ascii=False)
+            f.close()
+        os.system("cls")
+        print("Welcome, "+self.uname+"!")
+        time.sleep(1)
+        os.system("cls")
 
-        def create_github_repo(self, token, owner, repo_name, state):
-            self.url = "https://api.github.com/user/repos"
-            self.headers = {
-                "Authorization": f"token {token}",
-                "Accept": "application/vnd.github.v3+json"
-            }
-            self.data = {
-                "name": repo_name,
-                "private": state  # True yaparsan özel repo olur
-            }
-            self.response = requests.post(self.url, headers=self.headers, json=self.data)
+    def create_github_repo(self, token, owner, repo_name, state):
+        self.url = "https://api.github.com/user/repos"
+        self.headers = {
+            "Authorization": f"token {token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
+        self.data = {
+            "name": repo_name,
+            "private": state  # True yaparsan özel repo olur
+        }
+        self.response = requests.post(self.url, headers=self.headers, json=self.data)
 
-            if self.response.status_code == 201:
-                print(Fore.LIGHTYELLOW_EX+f"Repository created successfully: {Style.RESET_ALL+repo_name}")
-            else:
-                print(Fore.LIGHTYELLOW_EX+f"Failed to create repository: {Style.RESET_ALL+str(self.response.status_code)}")
-                print(self.response.json())
+        if self.response.status_code == 201:
+            print(Fore.LIGHTYELLOW_EX+f"Repository created successfully: {Style.RESET_ALL+repo_name}")
+        else:
+            print(Fore.LIGHTYELLOW_EX+f"Failed to create repository: {Style.RESET_ALL+str(self.response.status_code)}")
+            print(self.response.json())
 
-        def delete_github_repo(self, token, owner, repo_name):
-            self.url = f"https://api.github.com/repos/{owner}/{repo_name}"
-            self.headers = {
-                "Authorization": f"token {token}",
-                "Accept": "application/vnd.github.v3+json"
-            }
+    def delete_github_repo(self, token, owner, repo_name):
+        self.url = f"https://api.github.com/repos/{owner}/{repo_name}"
+        self.headers = {
+            "Authorization": f"token {token}",
+            "Accept": "application/vnd.github.v3+json"
+        }
 
-            self.response = requests.delete(self.url, headers=self.headers)
+        self.response = requests.delete(self.url, headers=self.headers)
 
-            if self.response.status_code == 204:
-                print(f"Repository successfully deleted: {Style.RESET_ALL+repo_name}")
-            else:
-                if self.response.status_code == 203:
-                    print(Fore.LIGHTYELLOW_EX+f"Repository could not be deleted: {Style.RESET_ALL}Make sure you grant token authorizations correctly.")
-                print(self.response.json())
-
-    if __name__ == '__main__':
-        term = terminal()
-except Exception as e:
-    print(Fore.RED+"Error: "+str(e))
+        if self.response.status_code == 204:
+            print(f"Repository successfully deleted: {Style.RESET_ALL+repo_name}")
+        else:
+            if self.response.status_code == 203:
+                print(Fore.LIGHTYELLOW_EX+f"Repository could not be deleted: {Style.RESET_ALL}Make sure you grant token authorizations correctly.")
+            print(self.response.json())
+if __name__ == '__main__':
+    term = terminal()
